40004a969e4f3605cac1efbb64d7e5f72551b49b
Ehtasham <ehtashamhusain@gmail.com>
2021-08-13 15:25:31 +0530

    UC-2 Rolls the Dice and get a number between 1-6

--- src/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator.java
@@ -1,5 +1,7 @@
 public class SnakeLadderSimulator {
     public static void main(String[] args) {
         int Player_Position = 0;
+        int PlayerRollsDice = (int)Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println(PlayerRollsDice);
     }
 }
