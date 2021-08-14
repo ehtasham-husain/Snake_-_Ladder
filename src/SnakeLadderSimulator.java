099cce5b70226b41da72bcd8dcf5f9108a7091de
Ehtasham <ehtashamhusain@gmail.com>
2021-08-14 23:05:20 +0530

    UC-4 Repeat till the player reaches the winning position 100

--- src/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator.java
@@ -4,21 +4,29 @@ public class SnakeLadderSimulator {
     public static final int Ladder = 2;
     public static void main(String[] args) {
         int Player_Position = 0;
-        int RollsDice = (int)Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println(RollsDice);
-        int Options = (int) Math.floor(Math.random() * 10) % 3;
-        System.out.println(Options);
-        switch (Options) {
-            case No_Play:
-                System.out.println("Player No Play");
-            case Snake:
-                Player_Position += RollsDice;
-                break;
-            case Ladder:
-                Player_Position -= RollsDice;
-                break;
-            default:
-                System.out.println("Some error occured");
+        while (Player_Position <= 100) {
+            int RollsDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            System.out.println(RollsDice);
+            int Options = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println(Options);
+            switch (Options) {
+                case No_Play:
+                    System.out.println("Player No Play::🚫🚫");
+                    break;
+                case Snake:
+                    System.out.println("SNAKE::🐍🐍");
+                    Player_Position -= RollsDice;
+                    break;
+                case Ladder:
+                    System.out.println("LADDER::🔗🔗");
+                    Player_Position += RollsDice;
+                    break;
+                default:
+                    System.out.println("Some error occured");
+            }
+            if (Player_Position < 0)
+                Player_Position = 0;
+            System.out.println("Player Position : " +Player_Position );
         }
     }
 }
